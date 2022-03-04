4f51737c0fc575488bc9e7f6d46b6ae4c34d7e1c
PC100-Fan <harald@haus-b.de>
2022-03-04 14:22:21 +0100

    Second test

--- module5-solution/spec/menu.service.spec.js
+++ module5-solution/spec/menu.service.spec.js
@@ -1,6 +1,6 @@
 describe('menuservice', function () {
 
-  var myMenuService;
+  var MenuService;
   var $httpBackend;
   var ApiPath;
 
@@ -10,7 +10,7 @@ describe('menuservice', function () {
     inject(function ($injector) {
       $httpBackend = $injector.get('$httpBackend');
       ApiPath = $injector.get('ApiPath');
-      myMenuService = $injector.get('MenuService');
+      MenuService = $injector.get('MenuService');
     });
   });
 
@@ -24,9 +24,23 @@ describe('menuservice', function () {
     var shortName = "A1";
     $httpBackend.whenGET(ApiPath + '/menu_items/' + shortName.toUpperCase() + '.json')
                 .respond({"id":1,"short_name":"A1","name":"Won Ton Soup with Chicken","description":"chicken-stuffed won tons in clear chicken broth with white meat chicken pieces and a few scallions","price_small":2.55,"price_large":5.0,"small_portion_name":"pint","large_portion_name":"quart","created_at":"2022-02-26T21:04:08.744Z","updated_at":"2022-02-26T21:04:08.744Z","category_short_name":"A","image_present":true});
-    myMenuService.getMenuItem(shortName).then(function(response) {
+    MenuService.getMenuItem(shortName).then(function(response) {
       expect(response.short_name).toEqual(shortName);
     });
+
+    $httpBackend.flush();
+  });
+
+  it('should return an error for non-existing shortName', function() {
+    var shortName = "xy";
+    $httpBackend.whenGET(ApiPath + '/menu_items/' + shortName.toUpperCase() + '.json')
+                .respond(500, '');
+    MenuService.getMenuItem(shortName).then(function(response) {
+      fail("Non-existing shortname should return an error!");
+    }).catch(function (reason) {
+      expect(parseInt(reason.status, 10)).toEqual(500);
+    });
+
     $httpBackend.flush();
   });
 
